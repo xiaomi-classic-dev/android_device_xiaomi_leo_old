3a3a2acdbf4e495435cf30167eba4867da7b8bf8
GuaiYiHu <guaiyihu@foxmail.com>
2017-09-18 12:06:51 +0800

    init twrp for mi note pro

---
+++ lineage.mk
@@ -0,0 +1,19 @@
+# Release name
+PRODUCT_RELEASE_NAME := leo
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/leo/device_leo.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := leo
+PRODUCT_NAME := lineage_leo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi Note Pro
+PRODUCT_MANUFACTURER := Xiaomi
