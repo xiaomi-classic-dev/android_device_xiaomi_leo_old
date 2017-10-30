b1128476159c4396741dde92f028147bf5e8f52e
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-30 23:43:38 +0800

    leo: Nuke sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,6 +224,6 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_leo
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += device/xiaomi/leo/sepolicy
+# BOARD_SEPOLICY_DIRS += device/xiaomi/leo/sepolicy
