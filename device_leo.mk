8bcdab5fcda192c93399becba085349d86818766
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-31 16:58:53 +0800

    leo: camera: Use camera.device@1.0-impl

--- device_leo.mk
+++ device_leo.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap \
     libshim_atomic \
-    camera.device@3.2-impl \
+    camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl
 
 # Keystore
