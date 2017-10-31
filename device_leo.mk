a976a8ac0f8ce9471c5b95efc50c8346cec3680d
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-31 17:35:45 +0800

    leo: Update seccomp policy

--- device_leo.mk
+++ device_leo.mk
@@ -191,8 +191,7 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
     
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
